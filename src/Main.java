75c416b59691bed715818f5223b554a6252f819c
mukesh007kls <mallikanti2000@gmail.com>
2023-02-28 09:23:38 +0530

    ADDED:Code for checking Prime Number

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,8 @@ public class Main {
 
     public static void main(String[] args) {
         Main m=new Main();
-        m.fibonacciSeries();
+        //m.fibonacciSeries();
+        m.primeNumber();
     }
     public void fibonacciSeries(){
         System.out.println("Enter how many numbers in the fibonacci series to print:- ");
@@ -22,6 +23,26 @@ public class Main {
             num2 = temp;
             System.out.print(" " + temp);
             i++;
+        }
+    }
+    public void primeNumber(){
+        System.out.println("Enter the number to check:- ");
+        int num= sc.nextInt();
+        int temp=num/2;
+        int count=1;
+        if(num==0||num==1){
+            System.out.println(num+" is not a prime");
+        }else{
+            for (int i = 2; i <=temp ; i++) {
+                if(num%i==0){
+                    count++;
+                }
+            }
+            if(count<=2){
+                System.out.println(num+" is a prime number");
+            }else{
+                System.out.println(num+" is not a prime number");
+            }
         }
     }
 
