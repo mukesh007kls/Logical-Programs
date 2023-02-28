fbadf035b4ba9d5a74939cfae06c69ef019451ff
mukesh007kls <mallikanti2000@gmail.com>
2023-02-28 23:49:36 +0530

    ADDED:Code to check Coupon numbers

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
@@ -8,19 +9,20 @@ public class Main {
         Main m = new Main();
         //m.fibonacciSeries();
         //m.primeNumber();
-       // m.reverseNumber();
-        m.perfectNumber();
+        // m.reverseNumber();
+        //m.perfectNumber();
+        m.couponNumbers();
     }
 
     public void fibonacciSeries() {
         System.out.println("Enter how many numbers in the fibonacci series to print:- ");
-        int limit = sc.nextInt();
+        int numberOfCoupon = sc.nextInt();
         int num1 = 0;
         int num2 = 1;
         int temp = 0;
         System.out.print(num1 + " " + num2);
         int i = 2;
-        while (i < limit) {
+        while (i < numberOfCoupon) {
             temp = num1 + num2;
             num1 = num2;
             num2 = temp;
@@ -77,6 +79,32 @@ public class Main {
         } else {
             System.out.println(num + " is not a perfect number");
         }
+    }
+
+    public void couponNumbers() {
+        System.out.println("Enter the number of coupons:-");
+        int numberOfCoupon = sc.nextInt();
+        int allCouponNumbers[] = new int[numberOfCoupon];
+        System.out.println("Enter all the coupon numbers:-");
+        for (int i = 0; i < numberOfCoupon; i++) {
+            allCouponNumbers[i] = sc.nextInt();
+        }
+        int numberOfMatches = 0;
+        int randomNumbersCount = 0;
+        Random r = new Random();
+        while (numberOfMatches < numberOfCoupon) {
+            randomNumbersCount++;
+            int randomCouponNumber = r.nextInt(20);
+            for (int j = 0; j < numberOfCoupon; j++) {
+                if (allCouponNumbers[j] == randomCouponNumber) {
+                    System.out.print(allCouponNumbers[j] + " ");
+                    numberOfMatches++;
+                    allCouponNumbers[j] = -2;
+                }
+            }
+        }
+        System.out.println();
+        System.out.println("Number of random coupon numbers generated are:- " + randomNumbersCount);
     }
 
 }
